f79a83c223eeca85f3e7e67b3109f241c7a9abc6
Dmytro Grendach <dmytro.grendach@beamtrail.com>
2022-03-14 12:27:55 +0400

    day1

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,8 @@
-# Variables
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
+
+#This code generate your band name based on your pet name and your city
+print("Welcome to the Band Name Generator.")
+street = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
