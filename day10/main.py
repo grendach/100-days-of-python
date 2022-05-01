aab54f2fcf4e258f34ebd2909c17551a42a830b1
Dmytro Grendach <dmytro.grendach@beamtrail.com>
2022-05-01 22:57:18 +0400

    day10

---
+++ day10/main.py
@@ -0,0 +1,92 @@
+#### Functions with output
+
+def format_names(f_name, l_name ):
+    """
+    Return formated first and last name
+    """
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs"
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+    print ("dupa")
+
+format_names()
+
+formated_string = format_names(input("What's your first name ? "), input("What's your last name ? "))
+print(formated_string)
+
+
+
+##### Days in Month #########
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(year) == True:
+      month_days[1] = 29
+
+  return month_days[month - 1]
+  
+  
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+#### Calculator
+from art import logo
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+
+      calculator()
+
+calculator()
