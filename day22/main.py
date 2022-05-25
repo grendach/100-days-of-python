f6f0148862a6c54eec9415ff7db7ddcfa6da5adc
Dmytro Grendach <dmytro.grendach@beamtrail.com>
2022-05-25 23:39:08 +0400

    day22

--- day22/main.py
+++ day22/main.py
@@ -1,5 +1,10 @@
 # 1. create a screen
 from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
@@ -7,11 +12,38 @@ screen.title("Pong Game")
 screen.tracer(0)
 # 2. Create and move  paddle #1
 # 3. Create and move  paddle #2
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
 # 4. Create the Ball and make it move
-# 5. Detect collisioin with the wall and bounce
-# 6. Detect collisioin with the ball
-# 7. Detect when the paddle misses
+ball = Ball()
 # 8. Keep score
+scoreboard = Scoreboard()
 # 6. Game over
 screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.ball_speed)
+    screen.update()
+    ball.move()
+    # 5. Detect collisioin with the wall and bounce
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # 6. Detect collisioin with the paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320 :
+        ball.bounce_x()
+    # 7. Detect when the paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 screen.exitonclick()
