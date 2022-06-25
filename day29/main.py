2c7724ae7d9d6a0971173f806270de002dd7d439
Dmytro Grendach <dmytro.grendach@beamtrail.com>
2022-06-25 17:50:30 +0400

    day29

--- day29/main.py
+++ day29/main.py
@@ -4,7 +4,6 @@ from random import choice, randint, shuffle
 import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
 #Password Generator Project
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -15,12 +14,13 @@ def generate_password():
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-    password_list = password_letters + password_symbols + password_numbers
-    shuffle(password_list)
+    your_password = password_letters + password_symbols + password_numbers
+    shuffle(your_password)
 
-    password = "".join(password_list)
+    password = "".join(your_password)
     password_entry.insert(0, password)
     pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -30,10 +30,10 @@ def save():
     password = password_entry.get()
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+        messagebox.showinfo(title="Opps", message="Please make sure that all fields are not empty")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                      f"\nPassword: {password} \nIs it ok to save?")
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the detailes entered: \nEmail: {email} "
+                                                  f"\nPassword: {password} \nIs it on to save?")
         if is_ok:
             with open("data.txt", "a") as data_file:
                 data_file.write(f"{website} | {email} | {password}\n")
@@ -44,36 +44,36 @@ def save():
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
-window.title("Password Manager")
+window.title("MyPassword")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
+canvas = Canvas(width=200, height=200)
+mypass_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=mypass_img)
+canvas.grid(column=1, row=0)
 
-#Labels
+# Labels
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
+website_label.grid(column=0, row=1)
+user_label = Label(text="Email:")
+user_label.grid(column=0, row=2)
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+password_label.grid(column=0, row=3)
 
-#Entries
+# Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(column=1, row=1, columnspan=2)
 website_entry.focus()
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "test@gmail.com")
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry.grid(column=1, row=3)
 
 # Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2)
+generate_password_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
